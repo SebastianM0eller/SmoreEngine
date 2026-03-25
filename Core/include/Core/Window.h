25ad0d39c9e2be43ad7d078199635054cb435f40
Sebastian <SebastianMOlesen@Gmail.com>
2026-03-25 16:03:25 +0100

    Implemented a virtual baseline Window class
    Fixed a typo in the root CMakeLists.txt
    Added a WindowConfig struct.
    Added some comments, for the Window and Config.

---
+++ Core/include/Core/Window.h
@@ -0,0 +1,40 @@
+// Smore - A Game Engine Project.
+// Copyright (c) 2026 Sebastian. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+namespace Smore::Core {
+
+///
+/// A struct containing the information neccecary to create a window.
+///
+struct WindowConfig {
+    std::string title = "Smore";
+    uint32_t width = 1280;
+    uint32_t height = 720;
+    bool VSync = true;
+};
+
+///
+/// A Virtual Window class, acting as a general interface for the rest of the Engine.
+///
+class Window {
+   public:
+    virtual ~Window() = default;
+
+    virtual void Update() = 0;
+
+    virtual bool ShouldClose() const noexcept = 0;
+
+    virtual uint32_t GetWidth() const noexcept = 0;
+    virtual uint32_t GetHeight() const noexcept = 0;
+
+    static std::unique_ptr<Window> Create(const WindowConfig& config = WindowConfig());
+};
+
+}  // namespace Smore::Core
