e9fb64c48d77fcde6da44ba9395c463274793671
Sebastian <SebastianMOlesen@Gmail.com>
2026-04-04 11:46:59 +0200

    Added the Input implementation for Headless

---
+++ Core/src/Platform/Headless/HeadlessInput.cpp
@@ -0,0 +1,17 @@
+// Smore - A Game Engine Project.
+// Copyright (c) 2026 Sebastian. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <Core/Input/Input.h>
+
+namespace Smore::Core {
+
+void Input::Init(Window*) noexcept { /* Do nothing */ }
+void Input::Update() noexcept { /* Do nothing */ }
+void Input::ClearKeys() noexcept { /* Do nothing */ }
+void Input::SyncKeys() noexcept { /* Do nothing */ }
+void Input::UpdateKey(KeyCode, bool) noexcept { /* Do nothing */ }
+bool Input::IsKeyPressed(KeyCode) noexcept { return false; }
+bool Input::IsKeyJustPressed(KeyCode) noexcept { return false; }
+
+}  // namespace Smore::Core
