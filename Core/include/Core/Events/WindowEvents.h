9a6e1601efd35a8493873f0fac836d104416354f
Sebastian <SebastianMOlesen@Gmail.com>
2026-04-06 11:00:33 +0200

    Added comments to the WindowEvents

--- Core/include/Core/Events/WindowEvents.h
+++ Core/include/Core/Events/WindowEvents.h
@@ -7,6 +7,9 @@
 
 namespace Smore::Core {
 
+///
+/// An event, dispatched when the user tried to close the window.
+///
 class WindowCloseEvent : public Event {
    public:
     WindowCloseEvent() noexcept = default;
@@ -15,12 +18,31 @@ class WindowCloseEvent : public Event {
     EVENT_CLASS_CATEGORY(Window);
 };
 
+///
+/// An event, dispatched when the window is resized.
+/// The event contains the new windowsize in pixels.
+///
 class WindowResizeEvent : public Event {
    public:
     WindowResizeEvent(uint32_t width, uint32_t height) noexcept : m_Width(width), m_Height(height) {}
 
+    ///
+    /// Returns the new width of the window.
+    /// The new width is in pixels, and returned as a uint32_t.
+    ///
     uint32_t GetWidth() const noexcept { return m_Width; }
+
+    ///
+    /// Returns the new height of the window.
+    /// The new height is in pixels, and returned as a uint32_t.
+    ///
     uint32_t GetHeight() const noexcept { return m_Height; }
+
+    ///
+    /// Returns the new size of the window, as a std::pair<uint32_t, uint32_t>
+    /// It is returned as (newWidth, newHeight).
+    ///
+    std::pair<uint32_t, uint32_t> GetSize() const noexcept { return {m_Width, m_Height}; }
 
     EVENT_CLASS_TYPE(WindowResize);
     EVENT_CLASS_CATEGORY(Window);
@@ -29,10 +51,19 @@ class WindowResizeEvent : public Event {
     uint32_t m_Width, m_Height;
 };
 
+///
+/// An event, dispatched when the window gains or looses focus.
+/// I contains a flag for wether the window gained focus, or lost it.
+/// Todo: Split into two events. One for gained, and one for lost.
+///
 class WindowFocusEvent : public Event {
    public:
     WindowFocusEvent(bool focus) : m_Focus(focus) {}
 
+    ///
+    /// Returns a boolean for wether the window gained or lost focus.
+    /// If the window gained focus, true is returned.
+    ///
     bool GetFocus() const noexcept { return m_Focus; }
 
     EVENT_CLASS_TYPE(WindowFocus);
