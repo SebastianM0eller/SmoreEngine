7f3902872b726a1e0880dc8781aaf3f725d2e9d2
Sebastian <SebastianMOlesen@gmail.com>
2026-04-18 09:38:17 +0200

    started working on the Renderer2D

---
+++ Renderer2D/include/Renderer2D/RendererManager.h
@@ -0,0 +1,15 @@
+// Smore - A Game Engine Project.
+// Copyright (c) 2026 Sebastian. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+#include <Core/Window.h>
+
+namespace Smore::Renderer2D {
+
+class RendererManager {
+   public:
+    RendererManager(Core::GraphicsAPI API);
+};
+
+}  // namespace Smore::Renderer2D
