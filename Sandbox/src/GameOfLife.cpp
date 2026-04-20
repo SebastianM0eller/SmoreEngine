44726b948f2249f35c4253d62a06f278118ca06a
Sebastian <SebastianMOlesen@gmail.com>
2026-04-20 09:26:53 +0200

    Added some comments for what some the the methods should do

--- Sandbox/src/GameOfLife.cpp
+++ Sandbox/src/GameOfLife.cpp
@@ -4,9 +4,27 @@
 
 #include "GameOfLife.h"
 
+#include "Core/DeltaTime.h"
+
 namespace GameOfLife {
 
 GameOfLifeLayer::GameOfLifeLayer(/* Add varaint, for how the initial grid is here*/) {
     // Generate the grid by random by default.
+}
+
+void GameOfLifeLayer::OnUpdate(Smore::Core::DeltaTime deltaTime) {
+    // Do the calculations and update the current board.
+    // It would be something along the lines of:
+    // For x if ... for y in... Should be for y, then for x, due to cache line.
+    // if Dead, update dead,
+    // else, update alive.
+
+    m_GameBoardPrevious = m_GameBoardCurrent;
+}
+
+void GameOfLifeLayer::OnRender() {
+    // Update a texutre, or create it, and send it to the GPU.
+    // Draw a fullscreen quad, with that texture, to render the scenes.
+}
 
 }  // namespace GameOfLife
