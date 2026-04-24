dc4ca4070afb13784811754f6ad3c75590e3f694
Sebastian <SebastianMOlesen@gmail.com>
2026-04-24 06:28:21 +0200

    Added methods to retrieve the amount of alive neighbours.

--- Sandbox/src/GameOfLife.cpp
+++ Sandbox/src/GameOfLife.cpp
@@ -1,30 +1,55 @@
 // Smore - A Game Engine Project.
 // Copyright (c) 2026 Sebastian. All rights reserved.
-// SPDX-License-Identifier: MIT
-
+//  SPDX-License-Identifier: MIT
 #include "GameOfLife.h"
 
-#include "Core/DeltaTime.h"
+#include <cstdint>
 
 namespace GameOfLife {
 
-GameOfLifeLayer::GameOfLifeLayer(/* Add varaint, for how the initial grid is here*/) {
-    // Generate the grid by random by default.
+GameOfLifeLayer::GameOfLifeLayer(
+    /* Add varaint, for how the initial grid is here*/) {
+  // Generate the grid by random by default.
 }
 
 void GameOfLifeLayer::OnUpdate(Smore::Core::DeltaTime deltaTime) {
-    // Do the calculations and update the current board.
-    // It would be something along the lines of:
-    // For x if ... for y in... Should be for y, then for x, due to cache line.
-    // if Dead, update dead,
-    // else, update alive.
+  // Do the calculations and update the current board.
+  // It would be something along the lines of:
+  // For x if ... for y in... Should be for y, then for x, due to cache line.
+  // if Dead, update dead,
+  // else, update alive.
 
-    m_GameBoardPrevious = m_GameBoardCurrent;
+  m_GameBoardPrevious = m_GameBoardCurrent;
 }
 
 void GameOfLifeLayer::OnRender() {
-    // Update a texutre, or create it, and send it to the GPU.
-    // Draw a fullscreen quad, with that texture, to render the scenes.
+  // Update a texutre, or create it, and send it to the GPU.
+  // Draw a fullscreen quad, with that texture, to render the scenes.
 }
 
-}  // namespace GameOfLife
+uint8_t GameOfLifeLayer::GetNeighbourCount(uint16_t x, uint16_t y) {
+  // Go through the surrounding squres, all sum up all the alive ones.
+  uint8_t alive = 0;
+  alive += IsAlive(x - 1, y + 1); // Top left,
+  alive += IsAlive(x, y + 1);     // Top middle,
+  alive += IsAlive(x + 1, y + 1); // Top right,
+  alive += IsAlive(x - 1, y);     // Middle left,
+  alive += IsAlive(x + 1, y);     // Middle right,
+  alive += IsAlive(x - 1, y - 1); // Buttom left,
+  alive += IsAlive(x, y - 1);     // Buttom middle,
+  alive += IsAlive(x + 1, y - 1); // Buttom right,
+  return alive;
+}
+
+bool GameOfLifeLayer::IsAlive(uint16_t x, uint16_t y) {
+  bool ignore = (x > m_GameBoardPrevious[0].size() ||
+                 y > m_GameBoardPrevious[0].size()) &&
+                m_Warping;
+
+  x = x % m_GameBoardPrevious[0].size();
+  y = y % m_GameBoardPrevious.size();
+
+  return m_GameBoardPrevious[y % m_GameBoardPrevious.size()][x];
+}
+
+} // namespace GameOfLife
--- Sandbox/src/GameOfLife.h
+++ Sandbox/src/GameOfLife.h
@@ -1,7 +1,6 @@
 // Smore - A Game Engine Project.
 // Copyright (c) 2026 Sebastian. All rights reserved.
 // SPDX-License-Identifier: MIT
-
 #pragma once
 #include <Core/DeltaTime.h>
 #include <Core/Events/Event.h>
@@ -23,6 +22,19 @@ class GameOfLifeLayer : public Smore::Runtime::Layer {
    private:
     std::array<std::array<bool, 1000>, 1000> m_GameBoardCurrent;
     std::array<std::array<bool, 1000>, 1000> m_GameBoardPrevious;
+
+    bool m_Warping;
+
+    ///
+    /// Returns the amount of alive squares around the provided location.
+    /// It takes the location, as x, y coordinates.
+    ///
+    uint8_t GetNeighbourCount(uint16_t x, uint16_t y);
+
+    ///
+    /// Returns true, if the square is alive, and false otherwise.
+    ///
+    bool IsAlive(uint16_t x, uint16_t y);
 };
 
 }  // namespace GameOfLife
